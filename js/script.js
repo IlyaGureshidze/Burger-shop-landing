5457394c4c5dcb9bc4c7bb7921105d92ab24aaea
Ilya Gureshidze <enigmaster90@gmail.com>
2017-08-21 00:18:32 +0300

    Fixed some js and added transition

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
-    $('.team__item').click(function(){
-        $('.team__item').removeClass('team__item_active');
-        $(this).addClass('team__item_active');
+    $('.team__item').click(function() {
+        //$('.team__item').removeClass('team__item_active');
+       if(!$(this).hasClass('team__item_active')) {                                    $(this).addClass('team__item_active').siblings().removeClass('team__item_active');
+        }
+        else {
+            $(this).removeClass('team__item_active');
+        }
     });
-    $('.menu-list__item').click(function(){
-        $('.menu-list__item').removeClass('menu-list__item_active');
-        $(this).addClass('menu-list__item_active');
+    $('.menu-list__item').click(function() {
+        if(!$(this).hasClass('menu-list__item_active')) {
+          $(this).addClass('menu-list__item_active').siblings().removeClass('menu-list__item_active');
+        }
+        else {
+            $(this).removeClass('menu-list__item_active');
+        }
     });
     $('.fullscreen-menu__close').click(function(){
         $('.fullscreen-menu').fadeOut(300);
