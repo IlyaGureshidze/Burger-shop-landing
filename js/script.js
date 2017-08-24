62a8736339ab39ca5bea56af6bcaf533aaa6bc18
Ilya Gureshidze <enigmaster90@gmail.com>
2017-08-24 17:07:47 +0300

    Builded OnePageScroll. But didnt fix the swipe movement yet

--- js/script.js
+++ js/script.js
@@ -21,4 +21,121 @@ $(document).ready(function() {
     $('.hamburger').click(function(){
         $('.fullscreen-menu').fadeIn(300);
     });
+   /* $(".slider").owlCarousel({
+            loop: true,
+            items: 1,
+            dots: false,
+            nav: true//$(this).find('.slider_container').size() > 1? true: false
+            
+    });*/
+    
+    /*---One Page Scroll---*/
+    
+$(function() {
+    var sections = $('.section'),
+    frame = $('.maincontent'),
+    inScroll = false;
+      var performTransition = function (sectionEq) {
+        if (!inScroll){
+        inScroll = true;
+            var position = (sectionEq * (-100)) + '%';
+            frame.css({
+              'transform': 'translateY(' + position + ')',
+              '-webkit-transform': 'translateY(' + position + ')'
+            })
+            
+            sections.eq(sectionEq).addClass('active')
+              .siblings().removeClass('active');
+
+            setTimeout(function () {
+              inScroll = false;
+             $('.nav-bar__item').eq(sectionEq).addClass('nav-bar__item_active')
+                .siblings().removeClass('nav-bar__item_active');
+            }, 800);
+        }
+      }
+
+      var defineSections = function (sections) {
+        var activeSection = sections.filter('.active');
+        return {
+          activeSection: activeSection,
+          nextSection: activeSection.next(),
+          prevSection: activeSection.prev()
+        }
+      }
+
+      var scrollToSection = function (direction) {
+        var section = defineSections(sections);
+        if (direction == 'up' && section.nextSection.length) { // секции поднимаются вверх
+          performTransition(section.nextSection.index());
+        }
+
+        if (direction == 'down' && section.prevSection.length) { //секции опускаются сверху
+          performTransition(section.prevSection.index());
+        }
+      }
+       $('.wrapper').on({
+            'wheel': function(e) {
+                var deltaY = e.originalEvent.deltaY,
+                direction = "";
+
+                var direction = deltaY > 0 
+                ? direction = 'up'
+                : direction = 'down';
+                scrollToSection(direction);
+            },
+
+            touchmove: function(e) {
+                e.preventDefault();
+            }
+       });
+        $(document).on('keydown', function (e) {
+            var section = defineSections(sections);
+            switch (e.keyCode) {
+                case 38: // стрелка вверх
+                    if (section.prevSection.length) {
+                        performTransition(section.prevSection.index());
+                    }
+                    break;
+                case 40: //стрелка вниз
+                    if (section.nextSection.length) {
+                        performTransition(section.nextSection.index());
+                    }
+                    break;
+            }
+
+        });
+    
+        $('[data-target]').on('click', function (e) {
+            
+            e.preventDefault();
+
+            var item = $(this),
+            itemEq = item.data('target');
+            $('.fullscreen-menu').fadeOut(300);
+            performTransition(itemEq);  
+            
+        });
+        
+        /* move to section */
+
+        $('.about__arrow').on('click', function(e) {
+            e.preventDefault();
+            performTransition(1);
+        });
+
+        $('.order-link').on('click', function(e) {
+            e.preventDefault();
+            performTransition(6); /*Index starts counting from 0, eq from 1. First link is second screen*/ 
+
+        });
+
+        /*if (isMobile) {
+            $(window).swipe({
+                swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+                    scrollToSection(direction);
+                }
+            });            
+        }*/
+    });
 });
